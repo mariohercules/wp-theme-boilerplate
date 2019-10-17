0d068cc75e825e83e204341641d97748298a2e38
Mario Hercules <mariohercules@hotmail.com>
2019-10-17 11:27:09 -0300

    Initial commit

---
+++ functions.php
@@ -0,0 +1,20 @@
+<?php
+
+function load_stylesheets() {
+    
+    wp_register_style('stylesheet', get_template_directory_uri() . '/style.css','', 1, 'All');
+    wp_enqueue_style('stylesheet');
+}
+
+add_action('wp_enqueue_scripts','load_stylesheets');
+
+function load_javascript() {
+
+    wp_register_script('custom', get_template_directory_uri() . '/app.js', 'jquery', 1, true);
+    wp_enqueue_script('custom');
+
+}
+
+add_action('wp_enqueue_scripts','load_javascript');
+
+?>
---
+++ header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+
+    <?php wp_head();?>
+
+</head>
+<body <?php body_class('test');?>>
+    
